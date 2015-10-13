041254385b25e8b02ff193f64584c6dae1508cf5
Bartek Brak <bartek.rychlicki@gmail.com>
2015-10-13 18:56:02 +0200

    Add author info to setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(
                 'foreign exchange reference rates',
     packages=find_packages(include=('gwaith*',)),
     version='2015.10.12.6',
+    author='Bartek Brak',
+    author_email='bartek.rychlicki@gmail.com',
     install_requires=[
         'feedparser==5.2.1',
-
     ],
     extras_require={
         'processors': ['python-dateutil==2.4.2']
