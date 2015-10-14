b68e609b746af6211a85493246242ba00a26f306
Bartek Brak <bartek.rychlicki@gmail.com>
2015-10-14 13:11:13 +0200

    Improve output of the manual testing command adding headers

--- bin/hand_test_lib_main.py
+++ bin/hand_test_lib_main.py
@@ -6,17 +6,24 @@ from gwaith import get_rates, processors
 
 only = ('PLN', 'GBP')
 
+
+def header(msg):
+    print('=' * 80 + '\r\t\t\t ' + msg + ' ')
+
+
+header('to_json')
 for data in get_rates(processor=processors.to_json, only=only):
     print(data)
 
+header('raw')
 for data in get_rates(processor=processors.raw, only=only):
     print(data)
 
+header('raw_python')
 for data in get_rates(processor=processors.raw_python, only=only):
     print(data)
 
-for data in get_rates(processor=processors.raw_python, only=only):
-    print(data)
+header('to_csv')
 writer = csv.writer(sys.stdout)
 writer.writerow(['Currency', 'Date', 'Rate'])
 for currency in get_rates(
